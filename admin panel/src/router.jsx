a7f37617915eaeab41f77482241556fe002d46d2
22arpitmaru <22arpitmaru@gmail.com>
2024-11-15 22:43:08 +0530

    Update router.jsx

--- admin panel/src/router.jsx
+++ admin panel/src/router.jsx
@@ -76,7 +76,7 @@ const router = createBrowserRouter([
                 element: <Community />
             },
             {
-                path: "/community",
+                path: "/communityTransfer",
                 element: <CommunityTransfer />
             },
             {
