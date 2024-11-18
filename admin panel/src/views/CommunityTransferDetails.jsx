89e3b0a93a623e854dbda34021714f26ed5e590d
22arpitmaru <22arpitmaru@gmail.com>
2024-11-18 23:03:54 +0530

    Update CommunityTransferDetails.jsx

--- admin panel/src/views/CommunityTransferDetails.jsx
+++ admin panel/src/views/CommunityTransferDetails.jsx
@@ -152,7 +152,15 @@ export default function CommunityDetails() {
                             <label htmlFor='Communities'> Enter User Phone</label>
                         </div>
                          <div className="col-lg-9">
-                            <input type='text' className='package-width input-border' id='community_id' name='community_id' value={Communities[0].id || ''} />
+                            <input
+    type="text"
+    className="package-width input-border"
+    id="community_id"
+    name="community_id"
+    value={Communities.length > 0 && Communities[0]?.id ? Communities[0].id : ''}
+    readOnly
+/>
+
 
                             <input type='number' className='package-width input-border' id='mobile_number' name='mobile_number' placeholder='Enter phone number' />
                         </div>
